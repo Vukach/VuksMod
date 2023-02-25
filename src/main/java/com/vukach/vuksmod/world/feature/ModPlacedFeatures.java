41a7ab98f7f5caff142c6bdfd3355929f61f1ca0
DancerVlt69 <dancervlt69@googlemail.com>
2023-02-25 22:41:11 +0100

    simplified MOD_ID usage

--- src/main/java/com/vukach/vuksmod/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/vukach/vuksmod/world/feature/ModPlacedFeatures.java
@@ -1,8 +1,5 @@
 package com.vukach.vuksmod.world.feature;
 
-import com.vukach.vuksmod.VukachMod;
-import com.vukach.vuksmod.block.ModBlocks;
-import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
@@ -12,6 +9,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
+
+import static com.vukach.vuksmod.VukachMod.MOD_ID;
 
 public class ModPlacedFeatures {
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
